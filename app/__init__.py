53d179ce522005134704b1e7b6d1c6b99f57dcf7
lamechy <lamechongondi88@gmail.com>
2020-06-18 17:00:45 +0300

    commit changes made to this file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask 
 from flask_bootstrap import Bootstrap
+#from app import error
+#from app import views
 from .config import DevConfig
 # Initializing application 
 app = Flask(__name__, instance_relative_config=True)
@@ -10,5 +12,5 @@ app.config.from_pyfile('config.py')
 
 # Initializing Flask extensions 
 bootstrap = Bootstrap(app)
-
+from app import error
 from app import views
--- app/request.py
+++ app/request.py
@@ -8,6 +8,25 @@ api_key = app.config['MOVIE_API_KEY']
 #Getting the movie base url 
 base_url = app.config["MOVIE_API_BASE_URL"]
 
+def get_movie(id):
+    get_movie_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
+
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+    return movie_object
+
 def get_movies(category):
     '''
     Function that gets the json response to our url request
@@ -23,6 +42,7 @@ def get_movies(category):
         if get_movies_response['results']:
             movie_results_list = get_movies_response['results']
             movie_results = process_results(movie_results_list)
+
 
     return movie_results
 
