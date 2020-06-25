91af8864e50d335e8ca63cc744999102e9f2b82e
lamechy <lamechongondi88@gmail.com>
2020-06-25 12:20:29 +0300

    Created application factory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,28 @@
 from flask import Flask 
 from flask_bootstrap import Bootstrap
-#from app import error
-#from app import views
-from .config import DevConfig
+from config import config_options
+
+bootstrap = Bootstrap()
+
+
 # Initializing application 
-app = Flask(__name__, instance_relative_config=True)
+def create_app(config_name):
 
-#Setting up configurations
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    app = Flask(__name__)
+    #Creating app configurations
+    app.config.from_object(config_options[config_name])
 
-# Initializing Flask extensions 
-bootstrap = Bootstrap(app)
-from app import error
-from app import views
+
+    # Initializing Flask extensions 
+    bootstrap.init_app(app)
+
+    # Registering  the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    #Setting config
+    from .requests import configure_request
+    configure_request(app)
+    # Will add views and forms 
+
+    return app
